acc152889f033094e94d555ca742a016a65dfce2
vincent55312 <81149894+vincent55312@users.noreply.github.com>
2023-06-22 11:36:13 +0200

    feat: test CD

---
+++ prisma/migrations/20230621143608_delete_test/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - The values [test] on the enum `User_userType` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE `User` MODIFY `userType` ENUM('seller', 'client', 'prospects', 'internal') NOT NULL DEFAULT 'client';
